4a4643775e7ce12a70eaf2fcf9d4deb738d7d3eb
DarkDream99 <denys.zaluzhnyi@gmail.com>
2019-01-27 15:00:13 +0200

    add visual checker for files and error tests

--- Delone_project/visualize/scripts/file_reader.js
+++ Delone_project/visualize/scripts/file_reader.js
@@ -4,18 +4,45 @@ function processFiles(files) {
 	reader.onload = function (e) {
 		let inputString = e.target.result;
 		rows = inputString.split('\n');
+		points = [];
+
+		if (rows.length > 100) {
+			alert("Количество точек не должно превышать 100");
+			return;
+		}
+
 		for (let ind = 0; ind < rows.length; ind += 1) {
 			nums = rows[ind].split(' ');
-			let x = parseFloat(nums[0]);
-			let y = parseFloat(nums[1]);
+			let x, y;
+			if (isNumeric(nums[0]) && isNumeric(nums[1])) {
+				x = parseFloat(nums[0]);
+				y = parseFloat(nums[1]);
+			} else {
+				alert("Параметры в файле должны быть численными значениями");
+				return;
+			}
+			
+			if (x < -2000 || x > 2000 || y < -2000 || y > 2000) {
+				alert("Значения координат не должны превышать по модулю 2000");
+				return;
+			}
 
-			addNewPoint(x, y);
+			points.push(new Point(x, y));
+		}
+
+		for (let ind = 0; ind < points.length; ++ind) {
+			addNewPoint(points[ind].coordX, points[ind].coordY);
 		}
 	};
+
 	reader.readAsText(file);
 }
 
 function showFileInput() {
 	var fileInput = document.getElementById('fileInput');
 	fileInput.click();
+}
+
+function isNumeric(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
 }
